c0ceaac1902e39951468e101dbea8dcaa862627a
shigemk2 <i.am.shige@gmail.com>
2016-11-03 20:33:38 +0900

    Add case classes

--- src/main/scala/com/example/ContentFilter.scala
+++ src/main/scala/com/example/ContentFilter.scala
@@ -2,5 +2,13 @@ package com.example
 
 import akka.actor._
 
+case class FilteredMessage(light: String, and: String, fluffy: String, message: String) {
+  override def toString: String = {
+    s"FilteredMessage(" + light + " " + and + " " + fluffy + " " + message + ")"
+  }
+}
+
+case class UnfilteredPayload(largePayload: String)
+
 object ContentFilterDriver {
 }
